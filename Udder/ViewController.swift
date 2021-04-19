45225d2c131b502e71a6e1b8fa05ad6adc06c3e3
Mangesh Kshirsagar <mangesh@geotrackers.com>
2021-04-19 17:09:10 +0530

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Mangesh on 19/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
